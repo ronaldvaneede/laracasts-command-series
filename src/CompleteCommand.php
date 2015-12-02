8a6399053b109300b28d0fb671d228a2d05b967a
Ronald van Eede <ronald@vaneede.com>
2015-12-02 21:56:56 +0100

    Tasks app

---
+++ src/CompleteCommand.php
@@ -0,0 +1,30 @@
+<?php namespace Acme;
+
+use Acme\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CompleteCommand extends Command {
+
+    public function configure()
+    {
+        $this->setName('complete')
+             ->setDescription('Complete a task by its id')
+             ->addArgument('id', InputArgument::REQUIRED);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $id = $input->getArgument('id');
+
+        $this->database->query(
+            'delete from tasks where id = :id',
+            compact('id')
+        );
+
+        $output->writeln('<info>Task Completed!</info>');
+
+        $this->showTasks($output);
+    }
+}
---
+++ src/ShowCommand.php
@@ -0,0 +1,19 @@
+<?php namespace Acme;
+
+use Acme\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ShowCommand extends Command {
+
+    public function configure()
+    {
+        $this->setName('show')
+             ->setDescription('Show all tasks');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->showTasks($output);
+    }
+}
